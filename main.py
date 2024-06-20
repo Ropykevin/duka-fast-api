001302ef5d9ef2d41c3d9e860f12de674c49cb9d
Ropy Kevin <ropykevin@gmail.com>
2024-06-20 10:05:45 -0900

    master

--- main.py
+++ main.py
@@ -31,11 +31,7 @@ app=FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "http://localhost:5173",
-        "http://209.38.36.208",
-    ],
+    allow_origins=['*'],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
